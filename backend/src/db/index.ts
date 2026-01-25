fb4e184685e4a1b6bf79a4ae5a69c68bf1a7cfca
Tirth-955 <ctirth955@gmail.com>
2026-01-25 23:38:51 +0530

    db schema added

---
+++ backend/src/db/index.ts
@@ -0,0 +1,21 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from "./schema";
+import { ENV } from '../config/env'
+
+if (!ENV.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+// initialize PostgreSQL connetion pool
+const pool = new Pool({ connectionString: ENV.DATABASE_URL });
+
+pool.on("connect", () => {
+    console.log("Database Connected Successfully");
+});
+
+pool.on("error", (err) => {
+    console.error("Database Connection Error", err);
+});
+
+export const db = drizzle({ client: pool, schema });
